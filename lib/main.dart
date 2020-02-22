464578b2abed8da4974eb6217fbf6998ef890c2a
naimishverma17 <itakgec@gmail.com>
2020-02-22 22:36:30 +0530

    edited pub spec

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import './pages_export.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Student Portal',
       initialRoute: '/',
+      theme: ThemeData(
+        fontFamily: 'Lato'
+      ),
       routes: {'/': (context) => SplashPage()},
     );
   }
--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-
+    return Scaffold(
+      body: Center(
+        child: Text('Hello'),
+      ),
     );
   }
 }
