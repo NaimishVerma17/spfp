0ad2e61c05fce9298d164f5f34ce71eb523ac8e6
naimishverma17 <itakgec@gmail.com>
2020-02-23 18:08:49 +0530

    exports for reminder pages

--- lib/pages_export.dart
+++ lib/pages_export.dart
@@ -4,3 +4,5 @@ export './pages/signup.dart';
 export './pages/forget_password.dart';
 export './pages/reset_password.dart';
 export './pages/home.dart';
+export './pages/reminder.dart';
+export './pages/add-reminder.dart';
