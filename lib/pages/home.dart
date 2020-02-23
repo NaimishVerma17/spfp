93374fa693e998b89ca4e6b16eba7d4b202f7205
naimishverma17 <itakgec@gmail.com>
2020-02-23 18:04:34 +0530

    home screen changes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,12 @@ import 'package:student_portal/widgets/drawer.dart';
 import 'package:student_portal/utils/styles.dart';
 
 class HomePage extends StatelessWidget {
+  String _intro = ''' 
+This app is for the students of Ajay Kumar Garg Engineering College, Ghaziabad.
+This App provides a personalized experience to the students as they can view their timetable, add notes and reminders about particular classes, and get notified about any changes in their schedule through in app notifications by just logging in  using their college provided email accounts and password. 
+Optimized way to communicate with students and provide them a better experience of their timetable - The Timetable App.
+  ''';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,6 +18,49 @@ class HomePage extends StatelessWidget {
         elevation: 3.0,
         backgroundColor: Colors.white,
         iconTheme: IconThemeData(color: Styles.primaryColor),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0,vertical: 10.0),
+        color: Styles.homeBackgroundColor,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Image(
+                  image: AssetImage('assets/images/book.png'),
+                  height: 200.0,
+                  width: 150.0,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Welcome to Timetable app for student!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w500,
+                    color: Styles.primaryColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  _intro,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text('v0.0.1',style: TextStyle(fontSize: 20.0),)
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
